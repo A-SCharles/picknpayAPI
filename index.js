ebf8a2c75042657ae607378781f63690e048c703
A-SCharles <a.charles.edu@gmail.com>
2022-08-05 08:38:51 +0200

    changed header

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const router = express.Router();
 const port = parseInt(process.env.PORT);
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.setHeader("Access-Control-Allow-Origin", "https://picknpay-apitest.herokuapp.com");
   next();
 });
 
